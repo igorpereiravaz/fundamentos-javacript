71d1adfa2b3114a8d52a7167ed6f3642b16c0948
igorvaz-pravaler <igor.vaz@pravaler.com.br>
2023-03-08 10:52:23 -0300

    Aula 1 e 2

---
+++ declaracao-variaveis/conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString  ='456';
+console.log(numero + numeroString);
+
+// conversao explicita
+// Number()
+// Srting()
+console.log(numero + Number(numeroString));
---
+++ declaracao-variaveis/types-var.js
@@ -0,0 +1,37 @@
+//var
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+// console.log(area);
+var area; 
+
+//let
+let forma = 'retangulo';
+let altura2 = 5;
+let comprimento2 = 7;
+let area2;
+
+if (forma === 'retangulo'){
+    area2 = altura2 * comprimento2;
+} else {
+    area2 = (altura2 * comprimento2) / 2;
+}
+
+// console.log(area2);
+
+// const
+const forma3 = 'triangulo';
+const altura3 = 5;
+const comprimento3 = 7;
+let area3; 
+
+// console.log(area3);
+
+if (forma === 'quadrado'){
+    area3 = altura3 * comprimento3;
+} else {
+    area3 = (altura3 * comprimento3) / 2;
+}
+
+console.log(area3);
